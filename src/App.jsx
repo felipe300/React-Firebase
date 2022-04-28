c1878c8efd51f13562cc63f4fa477a9bc45d3f1e
Felipe300 <caracoloro@gmail.com>
2022-04-28 00:25:16 -0400

    feat: Add firebase Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { UserContext } from './context/UserProvider'
 import RequiredAuth from './middlewares/RequiredAuth'
 
 import Home from './routes/Home'
 import Login from './routes/Login'
+import Register from './routes/Register'
 
 const App = () => {
+  const { user } = useContext(UserContext)
+
+  if (user === false) {
+    return <p>LOADING...</p>
+  }
+
   return (
     <>
       <Navbar />
@@ -20,6 +29,7 @@ const App = () => {
           }
         />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </>
   )
